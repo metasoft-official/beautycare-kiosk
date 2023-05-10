6de286064fb3215c5a8aa85d9a10764bc1d29b55
uurim <syl000126@naver.com>
2023-05-10 14:39:24 +0900

    design : debug banner 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-import 'package:go_router/go_router.dart';
 
 import 'package:riverpod_project/common/const/router.dart';
 
 void main() {
-  runApp(ProviderScope(
+  runApp(const ProviderScope(
       child: MyApp()
   ));
 }
@@ -18,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       routeInformationProvider: router.routeInformationProvider,
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
