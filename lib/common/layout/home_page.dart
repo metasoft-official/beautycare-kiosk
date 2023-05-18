dbeedeead1aab61ed6f9e691273eea916d0cae20
yeongsul <yeoungsul@gmail.com>
2023-05-18 14:10:38 +0900

    design: 로그인, 회원가입 메뉴 app bar로 이동

--- lib/common/layout/home_page.dart
+++ lib/common/layout/home_page.dart
@@ -1,33 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomePage'),
+        title: const Text('뷰티 케어'),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                onPressed: () => context.pushNamed('login'),
+                icon: const Icon(Icons.login),
+              ),
+              IconButton(
+                onPressed: () => context.pushNamed('register'),
+                icon: const Icon(Icons.person),
+              ),
+              // const SizedBox(height: 20),
+              // ElevatedButton(
+              //   onPressed: () => context.pushNamed('survey'),
+              //   child: const Text('설문조사'),
+              // ),
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () => context.pushNamed('login'),
-              child: const Text('로그인'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => context.pushNamed('register'),
-              child: const Text('회원가입'),
-            ),
-            // const SizedBox(height: 20),
-            // ElevatedButton(
-            //   onPressed: () => context.pushNamed('survey'),
-            //   child: const Text('설문조사'),
-            // ),
-          ],
-        ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // const SizedBox(height: 20),
+              // ElevatedButton(
+              //   onPressed: () => context.pushNamed('survey'),
+              //   child: const Text('설문조사'),
+              // ),
+            ],
+          ),
+        ],
       ),
     );
   }
