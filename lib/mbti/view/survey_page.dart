ffd889a34d563a9568dc604ae12442e6cbe5f1d5
uuurim <yrsin@meta-soft.co.kr>
2023-05-19 18:06:35 +0900

    feat : 문진 stepper 추가

--- lib/mbti/view/survey_page.dart
+++ lib/mbti/view/survey_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_project/common/component/widgets/icon_stepper.dart';
 
-class SurveyPage extends StatelessWidget {
+import '../../common/layout/app_color.dart';
+import '../../common/layout/app_text.dart';
+
+class SurveyPage extends ConsumerStatefulWidget {
   const SurveyPage({super.key});
 
   static String get routeName => 'survey';
+
+  @override
+  SurveyState createState() => SurveyState();
+}
+
+class SurveyState extends ConsumerState<SurveyPage> {
+  int index = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -12,15 +24,31 @@ class SurveyPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('설문'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Column(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.all(22),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [],
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  child: IconStepper(
+                    icons: const [
+                      Icons.water_drop,
+                      Icons.warning,
+                      Icons.colorize,
+                      Icons.waves
+                    ],
+                    width: MediaQuery.of(context).size.width - 30,
+                    color: AppColor.blue,
+                    curStep: index,
+                    titles: const ['수분', '민감', '색소침착', '주름'],
+                  ),
+                )
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
