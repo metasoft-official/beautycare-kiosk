b722a0c4f66155a07684258b136272aff7c24c37
uuurim <yrsin@meta-soft.co.kr>
2023-06-16 15:05:10 +0900

    feat : 이력관리 tabbar 추가
    - 피부질환, mbti 이력 구분 탭 추가
    - sliverList 위젯 스크롤뷰 개별 추가

--- lib/user/view/ prediction_history_page.dart
+++ lib/user/view/ prediction_history_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../common/layout/app_color.dart';
 import 'history_widget.dart';
 
 class PredictionHistoryPage extends ConsumerStatefulWidget {
@@ -12,19 +13,64 @@ class PredictionHistoryPage extends ConsumerStatefulWidget {
   PredictionHistoryState createState() => PredictionHistoryState();
 }
 
-class PredictionHistoryState extends ConsumerState<PredictionHistoryPage> {
+class PredictionHistoryState extends ConsumerState<PredictionHistoryPage>
+    with TickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+    tabController.addListener(() {
+      // Do something when tab is switched
+    });
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('예측 이력')),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 30),
-        child: CustomScrollView(slivers: [
-          HistoryWidget(
-            childCount: 15,
-            // todo 기존 이력 데이터 리스트로 연결
-          )
-        ]),
+      appBar: AppBar(
+        title: const Text('예측 이력'),
+        bottom: TabBar(
+          tabs: [
+            Container(
+              margin: const EdgeInsets.all(8),
+              child: const Text('피부 MBTI',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            ),
+            Container(
+              margin: const EdgeInsets.all(8),
+              child: const Text('피부 질환',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            )
+          ],
+          labelColor: AppColor.appColor,
+          indicatorColor: AppColor.appColor,
+          controller: tabController,
+          unselectedLabelColor: AppColor.grey,
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(16, 0, 16, 20),
+        height: MediaQuery.of(context).size.height,
+        child: TabBarView(
+          controller: tabController,
+          children: const [
+            // todo : mbti, 피부질환 이력 데이터에 맞게 전달
+            HistoryWidget(
+              itemCount: 10,
+            ),
+            HistoryWidget(
+              itemCount: 3,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/user/view/history_widget.dart
+++ lib/user/view/history_widget.dart
@@ -6,18 +6,18 @@ import '../../common/layout/app_text.dart';
 
 // 이미지와 내용이 Row 형태인 리스트 공통 위젯
 class HistoryWidget extends StatelessWidget {
-  const HistoryWidget(
-      {Key? key,
-      required this.childCount,
-      this.decoration,
-      this.margin,
-      this.padding,
-      this.imgUrl,
-      this.titles,
-      this.captions})
-      : super(key: key);
+  const HistoryWidget({
+    Key? key,
+    required this.itemCount,
+    this.decoration,
+    this.margin,
+    this.padding,
+    this.imgUrl,
+    this.titles,
+    this.captions,
+  }) : super(key: key);
 
-  final int childCount;
+  final int itemCount;
   final BoxDecoration? decoration;
   final EdgeInsets? margin;
   final EdgeInsets? padding;
@@ -28,59 +28,63 @@ class HistoryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-      delegate:
-          SliverChildBuilderDelegate(childCount: childCount, (context, index) {
-        return InkWell(
-          // 리스트 각 요소 선택 시 이벤트
-          onTap: () => context.pushNamed('mbtiResult'),
-          child: Container(
-            decoration:
-                decoration ?? AppBoxTheme.basicShadowWhiteBoxTheme, // 전체 박스
-            margin: margin ?? const EdgeInsets.only(top: 16),
-            padding: padding ?? const EdgeInsets.all(8),
-            child: Row(
-              children: [
-                // 이미지
-                if (imgUrl != null && imgUrl!.length >= childCount) ...[
-                  Container(
-                    decoration: AppBoxTheme.greyBoxTheme,
-                    padding: const EdgeInsets.all(16),
-                    // todo : network
-                    child: Image.asset(
-                        imgUrl?[index] ?? 'assets/images/sample_cat_01.png',
-                        width: 40),
-                  ),
-                  const SizedBox(width: 16),
-                ],
-
-                // img sample (지울 부분)
-                Container(
-                    decoration: AppBoxTheme.greyBoxTheme,
-                    padding: const EdgeInsets.all(16),
-                    child: Image.asset('assets/images/sample_cat_01.png',
-                        width: 40)),
-                const SizedBox(width: 16),
-
-                // 텍스트 내용 (null값 추후 수정)
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    return CustomScrollView(slivers: [
+      SliverList(
+        delegate:
+            SliverChildBuilderDelegate(childCount: itemCount, (context, index) {
+          return SizedBox(
+            child: InkWell(
+              // 리스트 각 요소 선택 시 이벤트
+              onTap: () => context.pushNamed('mbtiResult'),
+              child: Container(
+                decoration:
+                    decoration ?? AppBoxTheme.basicShadowWhiteBoxTheme, // 전체 박스
+                margin: margin ?? const EdgeInsets.only(top: 16),
+                padding: padding ?? const EdgeInsets.all(8),
+                child: Row(
                   children: [
-                    Text(
-                      titles?[index] ?? 'DRPT',
-                      style: AppTextTheme.black18b,
-                    ),
-                    Text(
-                      captions?[index] ?? '2023년 4월 17일',
-                      style: AppTextTheme.deepGrey16,
+                    // 이미지
+                    if (imgUrl != null && imgUrl!.length >= itemCount) ...[
+                      Container(
+                        decoration: AppBoxTheme.greyBoxTheme,
+                        padding: const EdgeInsets.all(16),
+                        // todo : network
+                        child: Image.asset(
+                            imgUrl?[index] ?? 'assets/images/sample_cat_01.png',
+                            width: 40),
+                      ),
+                      const SizedBox(width: 16),
+                    ],
+
+                    // img sample (지울 부분)
+                    Container(
+                        decoration: AppBoxTheme.greyBoxTheme,
+                        padding: const EdgeInsets.all(16),
+                        child: Image.asset('assets/images/sample_cat_01.png',
+                            width: 40)),
+                    const SizedBox(width: 16),
+
+                    // 텍스트 내용 (null값 추후 수정)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          titles?[index] ?? 'DRPT',
+                          style: AppTextTheme.black18b,
+                        ),
+                        Text(
+                          captions?[index] ?? '2023년 4월 17일',
+                          style: AppTextTheme.deepGrey16,
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
+                ),
+              ),
             ),
-          ),
-        );
-      }),
-    );
+          );
+        }),
+      )
+    ]);
   }
 }
