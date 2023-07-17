ba5af52d0f7ad179ba70d3da1758f02be4d3c438
uuurim <yrsin@meta-soft.co.kr>
2023-07-17 11:55:46 +0900

    design : 하단 버튼 전체 클릭 적용으로 변경

--- lib/common/component/widgets/button_bottom_navigation_bar.dart
+++ lib/common/component/widgets/button_bottom_navigation_bar.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class ButtonBottomNavigationBarWidget extends StatelessWidget {
   const ButtonBottomNavigationBarWidget(
       {super.key,
-      // required this.child,
       required this.buttonColor,
       required this.textStyle,
       required this.label,
@@ -22,21 +21,21 @@ class ButtonBottomNavigationBarWidget extends StatelessWidget {
     return SizedBox(
         width: double.infinity,
         height: 60,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(0),
-            color: buttonColor,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              if (icon != null) ...[icon!],
-              TextButton(
-                onPressed: onPressed,
-                child: Text(label, style: textStyle),
-              ),
-            ],
+        child: InkWell(
+          onTap: onPressed,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(0),
+              color: buttonColor,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (icon != null) ...[icon!],
+                Text(label, style: textStyle),
+              ],
+            ),
           ),
         ));
   }
