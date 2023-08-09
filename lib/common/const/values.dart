f0eef8252b3d79dca856e3e2b5fc65eb0b2560a5
urma <syl000126@naver.com>
2023-08-09 16:55:56 +0900

    feat : string 추가

--- lib/common/const/values.dart
+++ lib/common/const/values.dart
@@ -1,9 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-const String BASE_URL = 'http://10.0.2.2:9000/api';
+// const String BASE_URL = 'http://10.0.2.2:9000/api';
+const String BASE_URL = 'http://192.168.0.52:9000/api';
 
-// const String BASE_URL = 'http://192.168.0.101:9000/api';
+class Strings {
+  Strings._();
+
+  static const String imageUrl = "$BASE_URL/image/";
+
+  // static const String imageUrl = "$frontUrl/api/image/";
+  // static const String videoUrl = "$frontUrl/api/video/";
+  // static const String audioUrl = "$frontUrl/api/audio/";
+  // static const String fileUrl = "$frontUrl/api/integrated-file/";
+}
 
 final dioProvider = Provider<Dio>((ref) {
   final dio = Dio();
