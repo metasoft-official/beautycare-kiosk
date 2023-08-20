c6fbf6edeeadbf786e2420e50833a70fab0380c0
yeongsul <yeoungsul@gmail.com>
2023-08-20 18:33:14 +0900

    fix: ios 푸시알림 설정 변경

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,21 @@
 import UIKit
 import Flutter
+import FirebaseCore
 import NaverThirdPartyLogin
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
+
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         GeneratedPluginRegistrant.register(with: self)
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
@@ -22,6 +29,7 @@ import NaverThirdPartyLogin
         if (!applicationResult) {
            applicationResult = super.application(app, open: url, options: options)
         }
+
         return applicationResult
     }
 }
