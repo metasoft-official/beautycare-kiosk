1546a3a2c5274b48cca2064b2d6207b76e44b281
urma <syl000126@naver.com>
2023-08-24 11:24:52 +0900

    docs : 서버 설정

--- lib/common/const/values.dart
+++ lib/common/const/values.dart
@@ -2,8 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // const String BASE_URL = 'http://10.0.2.2:9000/api';
+const String BASE_URL = 'http://meta-soft.iptime.org:9000/api';
 // const String BASE_URL = 'http://192.168.0.52:9000/api';
-const String BASE_URL = 'http://192.168.35.113:9000/api';
+// const String BASE_URL = 'http://192.168.35.113:9000/api';
 
 class Strings {
   Strings._();
